94527bbc45688f5961f556de2cafcb6496719db7
Luis Quevedo R2H <luisquevedor2h@rv-clt-lquevedo.redventures.net>
2018-10-06 15:23:54 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/portfolio-react/precache-manifest.0725194944685bed5ba4d5762b68eca6.js"
+  "/portfolio-react/precache-manifest.be9a3a45281c23d898b8356b7160bc4c.js"
 );
 
 workbox.clientsClaim();
